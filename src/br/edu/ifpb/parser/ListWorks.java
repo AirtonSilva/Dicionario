2bc669fab1b613d1b1271476ea1f54520d7edf7c
Airton Silva Braz <airtonsilva268@gmail.com>
2016-04-11 17:22:38 -0300

    ListWorks funcionando

--- src/br/edu/ifpb/parser/ListWorks.java
+++ src/br/edu/ifpb/parser/ListWorks.java
@@ -1,29 +1,24 @@
 package br.edu.ifpb.parser;
 
 import org.jsoup.Jsoup;
-import org.jsoup.helper.Validate;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
-/**
- * Example program to list links from a URL.
- */
 public class ListWorks {
     public static void main(String[] args) throws IOException {
     	
         String url = "http://www.dicio.com.br/";
-        String nome = "definir/";
+        String nome = "catucar/";
         String complete = url.concat(nome);
-
+        
         System.out.println(complete);
         
-        print("Fetching %s...", complete);
+        print("Procurando em %s...", complete);
 
         Document doc = Jsoup.connect(complete).get();
-        Elements links = doc.select("p");
+        Elements links = doc.select("p"); 	//AQUI SE PROCURA PELA TAG HTML
         
         System.out.println(links);
     }
@@ -32,10 +27,4 @@ public class ListWorks {
         System.out.println(String.format(msg, args));
     }
 
-    private static String trim(String s, int width) {
-        if (s.length() > width)
-            return s.substring(0, width-1) + ".";
-        else
-            return s;
-    }
 }
