382678be31c72b70a248792e7406e361a43e7d63
Airton Silva Braz <airtonsilva268@gmail.com>
2016-04-11 17:46:25 -0300

    Alterações no Bean

---
+++ src/br/edu/ifpb/entidade/Palavre.java
@@ -0,0 +1,17 @@
+package br.edu.ifpb.entidade;
+
+public class Palavre {
+	
+	private String definicao;
+	
+	public Palavre() {}
+
+	public String getDefinicao() {
+		return definicao;
+	}
+
+	public void setDefinicao(String definicao) {
+		this.definicao = definicao;
+	}
+
+}
--- src/br/edu/ifpb/managedBean/Bean.java
+++ src/br/edu/ifpb/managedBean/Bean.java
@@ -3,9 +3,34 @@ package br.edu.ifpb.managedBean;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
+import br.edu.ifpb.entidade.Palavre;
+
 @RequestScoped
 @ManagedBean
 public class Bean {
 	
+	private Palavre palavra;
+	
+	public Palavre getPalavre() {
+		return palavra;
+	}
+	
+	public String buscar(){
+		
+		String definicao = null;
+		palavra.setDefinicao(definicao);
+		
+		System.out.println(palavra.getDefinicao());
+		return null;
+		
+	}
+
+	public Palavre getPalavra() {
+		return palavra;
+	}
+
+	public void setPalavra(Palavre palavra) {
+		this.palavra = palavra;
+	}
 	
 }
